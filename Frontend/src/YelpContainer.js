2a67512d880dc8ff3a3c26f2a484bf2ab99d3da5
febanda01 <febanda91@gmail.com>
2019-02-25 14:17:30 -0600

    retrieved yelp api data

--- Frontend/src/YelpContainer.js
+++ Frontend/src/YelpContainer.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { API_KEY } from "./constants";
+
+
+
 
 const config = {
-    headers: `Bearer ${process.env.YELP_API_KEY}`,
+    method: 'GET',
+    headers: {'Authorization': `Bearer ${API_KEY}`},
     params: {
         term: 'burgers',
-
+        location: '708 main st'
     }
-    
-
 }
 
 export class YelpContainer extends Component {
 
 
-    componentDidMount(){
-        axios.get('https://api.yelp.com/v3/businesses/search', config)
-        .then(res => console.log(res))
-    }
+  componentDidMount(){
 
-    render() {
-        return (
-            <div>
-                <h1>Yelp API first request</h1>
-            </div>
-        );
-    }
+      axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', config)
+      .then(res => console.log(res))
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Yelp API first request</h1>
+      </div>
+    );
+  }
 }
 
 export default YelpContainer;
