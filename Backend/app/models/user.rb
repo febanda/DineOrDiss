97682f6477cfc498fb0702e2817818cf244d62e5
febanda01 <febanda91@gmail.com>
2019-05-20 22:22:40 -0500

    updated models

--- Backend/app/models/user.rb
+++ Backend/app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :matches 
 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+
     def token 
         JWT.encode({user_id: self.id}, 'asdf')
         
